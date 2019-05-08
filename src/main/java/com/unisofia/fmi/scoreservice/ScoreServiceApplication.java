f379217a83f55be30314eb9f70ec2aacac15e18b
Alexander Nikolov <alexander.nikolov@absolutns.com>
2019-05-09 00:41:06 +0300

    transform to war

--- src/main/java/com/unisofia/fmi/scoreservice/ScoreServiceApplication.java
+++ src/main/java/com/unisofia/fmi/scoreservice/ScoreServiceApplication.java
@@ -2,11 +2,18 @@ package com.unisofia.fmi.scoreservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ScoreServiceApplication {
+public class ScoreServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScoreServiceApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ScoreServiceApplication.class);
+    }
 }
